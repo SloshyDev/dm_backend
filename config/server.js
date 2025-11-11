5324d4e1ac642b8b79e3d1ab7c802524d3bbfcb3
SLOSHYMX <devlnicanorl@gmail.com>
2025-11-11 09:40:18 -0600

    Primer commit

--- config/server.js
+++ config/server.js
@@ -1,10 +1,20 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  // Permitir host externo para Strapi
+  url: env("PUBLIC_URL", "https://api.yokaicdmx.com"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+    // Permitir acceso al admin desde el host externo
+    serveAdminPanel: true,
+    url: "/admin",
   },
 });
